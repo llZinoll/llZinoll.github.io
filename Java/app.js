b82b190642ffc651aff93d03972c5cab50b01c87
llZinoll <zinovanthek@gmail.com>
2023-06-06 15:49:41 +0200

    responsive

--- Java/app.js
+++ Java/app.js
@@ -1,13 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
     const carouselItems = document.querySelectorAll(".carousel-item");
-    const interval = 3000; // Intervalo de tiempo entre las transiciones (en milisegundos)
+    const interval = 5000; // Intervalo de tiempo entre las transiciones (en milisegundos)
     let currentItemIndex = 0;
-
+  
     function showNextItem() {
       carouselItems[currentItemIndex].classList.remove("active");
       currentItemIndex = (currentItemIndex + 1) % carouselItems.length;
       carouselItems[currentItemIndex].classList.add("active");
     }
-
+  
+    function showPreviousItem() {
+      carouselItems[currentItemIndex].classList.remove("active");
+      currentItemIndex = (currentItemIndex - 1 + carouselItems.length) % carouselItems.length;
+      carouselItems[currentItemIndex].classList.add("active");
+    }
+  
     setInterval(showNextItem, interval);
+  
+    const prevButton = document.getElementById("prevButton");
+    const nextButton = document.getElementById("nextButton");
+  
+    prevButton.addEventListener("click", showPreviousItem);
+    nextButton.addEventListener("click", showNextItem);
   });
