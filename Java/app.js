1a7213277e7a2aad95ce132f009cd1a76ad72a6c
llZinoll <zinovanthek@gmail.com>
2023-10-01 21:49:51 +0200

    Update

--- Java/app.js
+++ Java/app.js
@@ -1,25 +1,40 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const carouselItems = document.querySelectorAll(".carousel-item");
-    const interval = 5000; // Intervalo de tiempo entre las transiciones (en milisegundos)
-    let currentItemIndex = 0;
-  
-    function showNextItem() {
-      carouselItems[currentItemIndex].classList.remove("active");
-      currentItemIndex = (currentItemIndex + 1) % carouselItems.length;
-      carouselItems[currentItemIndex].classList.add("active");
-    }
-  
-    function showPreviousItem() {
-      carouselItems[currentItemIndex].classList.remove("active");
-      currentItemIndex = (currentItemIndex - 1 + carouselItems.length) % carouselItems.length;
-      carouselItems[currentItemIndex].classList.add("active");
-    }
-  
-    setInterval(showNextItem, interval);
-  
-    const prevButton = document.getElementById("prevButton");
-    const nextButton = document.getElementById("nextButton");
-  
-    prevButton.addEventListener("click", showPreviousItem);
-    nextButton.addEventListener("click", showNextItem);
-  });
+const slider = document.querySelector('#slider');
+let sliderSection = document.querySelectorAll('.slider__section');
+let sliderSectionLast = sliderSection[sliderSection.length -1];
+
+const btnLeft = document.querySelector('#btn-left');
+const btnRight = document.querySelector('#btn-right');
+
+slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+
+function Next() {
+  let sliderSectionFirst =  document.querySelectorAll('.slider__section')[0];
+  slider.style.marginLeftSSS = '-200%';
+  slider.style.trasnsition = 'all 0.5s';
+  setTimeout(function(){
+    slider.style.trasnsition = 'none';
+    slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+    slider.style.marginLeft = '-100%';
+  }, 500);
+}
+
+function Prev() {
+  let sliderSection = document.querySelectorAll('.slider__section');
+  let sliderSectionLast = sliderSection[sliderSection.length -1];
+  slider.style.marginLeftSSS = '';
+  slider.style.trasnsition = 'all 0.5s';
+  setTimeout(function(){
+    slider.style.trasnsition = 'none';
+    slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+    slider.style.marginLeft = '-100%';
+  }, 500);
+}
+
+
+btnRight.addEventListener('click', function(){
+  Next();
+});
+
+btnLeft.addEventListener('click', function(){
+  Prev();
+});
